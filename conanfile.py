614a446560d2572ceb7de55b77d68158446d4660
Jerry W <jerrywiltse@gmail.com>
2017-09-29 19:57:01 -0400

    standardize references on same channel

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class BoostGenerator(ConanFile):
     description = "Conan build generator for boost libraries http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "BSL"
     exports = "boostcpp.jam", "jamroot.template", "project-config.template.jam"
-    requires = "Boost.Build/1.64.0@bincrafters/stable"
+    requires = "Boost.Build/1.64.0@bincrafters/testing"
 
     def package_info(self):
         self.user_info.b2_command = "b2 -j%s -a --hash=yes --debug-configuration"%(tools.cpu_count())
